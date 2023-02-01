57d1d8316ae41d6d079776fd7f429fd1524279e7
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-02-01 07:56:41 +0100

    feat(components): starts adding SentinelStats

---
+++ src/components/complex/SentinelStats/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { Col, Row } from 'react-bootstrap'
+
+import Box from '../../base/Box'
+
+const SentinelStats = () => {
+  return <Box></Box>
+}
+
+export default SentinelStats
