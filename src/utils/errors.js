cce22174c9e65161ece3f39516e3dfb2480cf5d1
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-02-06 14:41:19 +0100

    refactor(utils): adds another error message to isValidError

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,2 +1,4 @@
 export const isValidError = (_msg) =>
-  !_msg.includes('user rejected transaction') && !_msg.includes('User denied to sign a transaction')
+  !_msg.includes('user rejected transaction') &&
+  !_msg.includes('User denied to sign a transaction') &&
+  !_msg.includes('internal json-rpc error')
