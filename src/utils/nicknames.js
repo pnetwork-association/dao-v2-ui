f37aec6c75362420e4985179539ca3ecd693876f
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-02-10 14:02:12 +0100

    feat(utils): adds hardcoded nickname for pNetwork Community Association

--- src/utils/nicknames.js
+++ src/utils/nicknames.js
@@ -1,7 +1,8 @@
 import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator'
 
 const EXCEPTIONS = {
-  '0xdd92eb1478d3189707ab7f4a5ace3a615cdd0476': 'DAO Treasury'
+  '0xdd92eb1478d3189707ab7f4a5ace3a615cdd0476': 'DAO Treasury',
+  '0xf1f6568a76559d85cf68e6597fa587544184dd46': 'pNetwork Community Association'
 }
 
 const getNickname = (_address) => {
