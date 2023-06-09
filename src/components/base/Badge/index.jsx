da219d8b03320d537f68eb164cb8d69464b5afba
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-06-09 15:01:38 +0200

    refactor(components): rm padding from badge

--- src/components/base/Badge/index.jsx
+++ src/components/base/Badge/index.jsx
@@ -6,7 +6,7 @@ import Text from '../Text'
 const BadgeOuter = styled.div`
   background-color: ${({ theme }) => theme.blue};
   border-radius: 4px;
-  padding: 2px 4px;
+  padding: 0px 4px;
 `
 
 const Badge = ({ text, ..._props }) => {
