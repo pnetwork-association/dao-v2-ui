cfe6aa63eea70bff880112a1d84495d1618d9cd5
envin <envin@proton.me>
2024-01-23 00:34:37 +0100

    refactor(voting-scripts): compute script encoding using viem

--- src/utils/voting-scripts.js
+++ src/utils/voting-scripts.js
@@ -1,16 +1,13 @@
-// window.Buffer = window.Buffer || require('buffer').Buffer
-// import abi from 'ethereumjs-abi'
-
-// TODO adapt for vite
+import { encodeAbiParameters, parseAbiParameters } from 'viem'
 
 const createExecutorId = (id) => `0x${String(id).padStart(8, '0')}`
 const encodeCallScript = (actions, specId = 1) => {
+  console.log('actions', actions)
   return actions.reduce((script, { to, calldata }) => {
-    // const addr = abi.rawEncode(['address'], [to]).toString('hex')
-    // const length = abi.rawEncode(['uint256'], [(calldata.length - 2) / 2]).toString('hex')
-    // // Remove 12 first 0s of padding for addr and 28 0s for uint32
-    // return script + addr.slice(24) + length.slice(56) + calldata.slice(2)
-    return 'this is a mockup'
+    const addr = encodeAbiParameters(parseAbiParameters('address'), [to]).substring(2)
+    const length = encodeAbiParameters(parseAbiParameters('uint256'), [(calldata.length - 2) / 2]).substring(2)
+    // Remove 12 first 0s of padding for addr and 28 0s for uint32
+    return script + addr.slice(24) + length.slice(56) + calldata.slice(2)
   }, createExecutorId(specId))
 }
 
