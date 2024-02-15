81b1e914b3abb987ea69fb5dd6a491dec3c53fd6
envin <envin@proton.me>
2024-02-15 16:35:48 +0100

    fix(forwarder): fix viem calls

--- src/utils/preparers/forwarder.js
+++ src/utils/preparers/forwarder.js
@@ -1,9 +1,7 @@
-import { encodeAbiParameters, encodeFunctionData } from 'viem'
+import { encodeAbiParameters, encodeFunctionData, parseAbi, parseAbiParameters } from 'viem'
 import BigNumber from 'bignumber.js'
 
 import settings from '../../settings'
-
-const encode = (...params) => new encodeAbiParameters(...params)
 
 const subtractFee = (_amount) => {
   const amountBn = BigNumber(_amount.toString())
@@ -13,47 +11,41 @@ const subtractFee = (_amount) => {
 const getForwarderLendUserData = ({ amount, duration, receiverAddress }) => {
   const amountWithoutFees = subtractFee(amount)
 
-  return encode(
-    ['address[]', 'bytes[]'],
+  return encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.pntOnGnosis, settings.contracts.lendingManager],
     [
-      [settings.contracts.pntOnGnosis, settings.contracts.lendingManager],
-      [
-        encodeFunctionData({
-          abi: ['function approve(address spender, uint256 amount)'],
-          functionName: 'approve',
-          args: [settings.contracts.lendingManager, amountWithoutFees]
-        }),
-        encodeFunctionData({
-          abi: ['function lend(address receiver, uint256 amount, uint64 duration)'],
-          functionName: 'lend',
-          args: [receiverAddress, amountWithoutFees, duration]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function approve(address spender, uint256 amount)']),
+        functionName: 'approve',
+        args: [settings.contracts.lendingManager, amountWithoutFees]
+      }),
+      encodeFunctionData({
+        abi: parseAbi(['function lend(address receiver, uint256 amount, uint64 duration)']),
+        functionName: 'lend',
+        args: [receiverAddress, amountWithoutFees, duration]
+      })
     ]
-  )
+  ])
 }
 
 const getForwarderStakeUserData = ({ amount, duration, receiverAddress }) => {
   const amountWithoutFees = subtractFee(amount)
 
-  return encode(
-    ['address[]', 'bytes[]'],
+  return encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.pntOnGnosis, settings.contracts.stakingManager],
     [
-      [settings.contracts.pntOnGnosis, settings.contracts.stakingManager],
-      [
-        encodeFunctionData({
-          abi: ['function approve(address spender, uint256 amount)'],
-          functionName: 'approve',
-          args: [settings.contracts.stakingManager, amountWithoutFees]
-        }),
-        encodeFunctionData({
-          args: ['function stake(address receiver, uint256 amount, uint64 duration)'],
-          functionName: 'stake',
-          args: [receiverAddress, amountWithoutFees, duration]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function approve(address spender, uint256 amount)']),
+        functionName: 'approve',
+        args: [settings.contracts.stakingManager, amountWithoutFees]
+      }),
+      encodeFunctionData({
+        abi: parseAbi(['function stake(address receiver, uint256 amount, uint64 duration)']),
+        functionName: 'stake',
+        args: [receiverAddress, amountWithoutFees, duration]
+      })
     ]
-  )
+  ])
 }
 
 const getForwarderUnstakeUserData = ({
@@ -62,106 +54,88 @@ const getForwarderUnstakeUserData = ({
   receiverAddress,
   contractAddress = settings.contracts.stakingManager
 }) =>
-  encode(
-    ['address[]', 'bytes[]'],
+  encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [contractAddress],
     [
-      [contractAddress],
-      [
-        encodeFunctionData({
-          abi: ['function unstake(address owner, uint256 amount, bytes4 chainId)'],
-          functionName: 'unstake',
-          args: [receiverAddress, amount, chainId]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function unstake(address owner, uint256 amount, bytes4 chainId)']),
+        functionName: 'unstake',
+        args: [receiverAddress, amount, chainId]
+      })
     ]
-  )
+  ])
 
 const getForwarderUpdateSentinelRegistrationByStakingUserData = ({ amount, duration, ownerAddress, signature }) => {
   const amountWithoutFees = subtractFee(amount)
 
-  return encode(
-    ['address[]', 'bytes[]'],
+  return encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.pntOnGnosis, settings.contracts.registrationManager],
     [
-      [settings.contracts.pntOnGnosis, settings.contracts.registrationManager],
-      [
-        encodeFunctionData({
-          abi: ['function approve(address spender, uint256 amount)'],
-          functionName: 'approve',
-          args: [settings.contracts.registrationManager, amountWithoutFees]
-        }),
-        encodeFunctionData({
-          abi: [
-            'function updateSentinelRegistrationByStaking(address owner, uint256 amount, uint64 duration, bytes signature)'
-          ],
-          functionName: 'updateSentinelRegistrationByStaking',
-          args: [ownerAddress, amountWithoutFees, duration, signature]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function approve(address spender, uint256 amount)']),
+        functionName: 'approve',
+        args: [settings.contracts.registrationManager, amountWithoutFees]
+      }),
+      encodeFunctionData({
+        abi: parseAbi([
+          'function updateSentinelRegistrationByStaking(address owner, uint256 amount, uint64 duration, bytes signature)'
+        ]),
+        functionName: 'updateSentinelRegistrationByStaking',
+        args: [ownerAddress, amountWithoutFees, duration, signature]
+      })
     ]
-  )
+  ])
 }
 
 const getForwarderVoteUserData = ({ voterAddress, id, vote }) =>
-  encode(
-    ['address[]', 'bytes[]'],
+  encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.dandelionVotingV3],
     [
-      [settings.contracts.dandelionVotingV3],
-      [
-        encodeFunctionData({
-          abi: ['function delegateVote(address voter, uint256 _voteId, bool _supports)'],
-          functionName: 'delegateVote',
-          args: [voterAddress, id, vote]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function delegateVote(address voter, uint256 _voteId, bool _supports)']),
+        functionName: 'delegateVote',
+        args: [voterAddress, id, vote]
+      })
     ]
-  )
+  ])
 
 const getForwarderUpdateSentinelRegistrationByBorrowingUserData = ({ ownerAddress, numberOfEpochs, signature }) =>
-  encode(
-    ['address[]', 'bytes[]'],
+  encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.registrationManager],
     [
-      [settings.contracts.registrationManager],
-      [
-        encodeFunctionData({
-          abi: [
-            'function updateSentinelRegistrationByBorrowing(address owner, uint16 numberOfEpochs, bytes signature)'
-          ],
-          functionName: 'updateSentinelRegistrationByBorrowing',
-          args: [ownerAddress, numberOfEpochs, signature]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi([
+          'function updateSentinelRegistrationByBorrowing(address owner, uint16 numberOfEpochs, bytes signature)'
+        ]),
+        functionName: 'updateSentinelRegistrationByBorrowing',
+        args: [ownerAddress, numberOfEpochs, signature]
+      })
     ]
-  )
+  ])
 
 const getForwarderIncreaseDurationLendUserData = ({ duration }) =>
-  encode(
-    ['address[]', 'bytes[]'],
+  encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.lendingManager],
     [
-      [settings.contracts.lendingManager],
-      [
-        encodeFunctionData({
-          abi: ['function increaseDuration(uint64 duration)'],
-          functionName: 'increaseDuration',
-          args: [duration]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function increaseDuration(uint64 duration)']),
+        functionName: 'increaseDuration',
+        args: [duration]
+      })
     ]
-  )
+  ])
 
 const getForwarderIncreaseStakingSentinelRegistrationDurationUserData = ({ duration }) =>
-  encode(
-    ['address[]', 'bytes[]'],
+  encodeAbiParameters(parseAbiParameters('address[], bytes[]'), [
+    [settings.contracts.lendingManager],
     [
-      [settings.contracts.lendingManager],
-      [
-        encodeFunctionData({
-          abi: ['function increaseSentinelRegistrationDuration(uint64 duration)'],
-          functionName: 'increaseSentinelRegistrationDuration',
-          args: [duration]
-        })
-      ]
+      encodeFunctionData({
+        abi: parseAbi(['function increaseSentinelRegistrationDuration(uint64 duration)']),
+        functionName: 'increaseSentinelRegistrationDuration',
+        args: [duration]
+      })
     ]
-  )
+  ])
 
 export {
   getForwarderIncreaseDurationLendUserData,
